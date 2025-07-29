0f0b7ebd9a98329780500d1a8a67b0fb6a45f692
edersonrincel <edersonrincel@gmail.com>
2025-07-29 17:47:17 -0300

    Alteração na versão do cache do service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -38,7 +38,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v4'; // Versão do cache incrementada
+    const CACHE_NAME = 'viagem-app-cache-v5'; // Versão do cache incrementada
     const URLS_TO_CACHE = [
       '/',
       'index.html',

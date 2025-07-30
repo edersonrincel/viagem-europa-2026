1bdaf7437f7cfacb264410fc7d821687f549d03a
Ederson R Rincel <edersonrincel@MacBook-Air-de-Ederson.local>
2025-07-30 13:05:32 -0300

    Reversão teste podcast

--- service-worker.js
+++ service-worker.js
@@ -38,7 +38,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v15'; // Versão do cache incrementada
+    const CACHE_NAME = 'viagem-app-cache-v16'; // Versão do cache incrementada
     const URLS_TO_CACHE = [
       '/',
       'index.html',

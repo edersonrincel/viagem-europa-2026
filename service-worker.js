6af9ba12a2ebb80c7a01b5c7800a501ad3940910
Ederson R Rincel <edersonrincel@MacBook-Air-de-Ederson.local>
2025-07-30 20:23:04 -0300

    Melhoria visual

--- service-worker.js
+++ service-worker.js
@@ -38,7 +38,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v20'; // Versão do cache incrementada
+    const CACHE_NAME = 'viagem-app-cache-v21'; // Versão do cache incrementada
     const URLS_TO_CACHE = [
       '/',
       'index.html',

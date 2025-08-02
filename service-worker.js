d2606d2a108ee4625f8acd671ecc92f5f92ee86d
Ederson R Rincel <edersonrincel@MacBook-Air-de-Ederson.local>
2025-08-02 20:38:27 -0300

    Adição do orçamento diário para a viagem

--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v1.4'; 
+    const CACHE_NAME = 'viagem-app-cache-v1.5'; 
     const URLS_TO_CACHE = [
         '/',
         '/index.html',

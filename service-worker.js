cab725f69b72c586d8b8e8822716fcb20329f19a
Ederson R Rincel <edersonrincel@MacBook-Air-de-Ederson.local>
2025-08-03 10:58:40 -0300

    Ajustes visuais orçamento diário

--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v1.12'; 
+    const CACHE_NAME = 'viagem-app-cache-v1.13'; 
     const URLS_TO_CACHE = [
         '/',
         '/index.html',

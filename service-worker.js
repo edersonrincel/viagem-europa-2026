f2e4556deb2959454ef55c1b4899b73bfd12e17e
Ederson R Rincel <edersonrincel@MacBook-Air-de-Ederson.local>
2025-08-03 11:53:38 -0300

    Mais um ajuste orçamento diário

--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v1.18'; 
+    const CACHE_NAME = 'viagem-app-cache-v1.19'; 
     const URLS_TO_CACHE = [
         '/',
         '/index.html',

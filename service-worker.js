d4aee2655b1b799d1e059e5586f4c0f279fe4dad
Ederson R Rincel <edersonrincel@MacBook-Air-de-Ederson.local>
2025-08-03 11:41:04 -0300

    Ajuste botão orçamento diário

--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v1.16'; 
+    const CACHE_NAME = 'viagem-app-cache-v1.17'; 
     const URLS_TO_CACHE = [
         '/',
         '/index.html',

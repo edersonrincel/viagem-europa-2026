d48bb1bf5c1d1c539b886b3409a6ecec6bb859e9
Ederson R Rincel <edersonrincel@MacBook-Air-de-Ederson.local>
2025-08-07 00:40:33 -0300

    Ocultar Paris

--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v1.37'; 
+    const CACHE_NAME = 'viagem-app-cache-v1.38'; 
     const URLS_TO_CACHE = [
         '/',
         '/index.html',

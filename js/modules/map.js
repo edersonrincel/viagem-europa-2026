c16acedc4b30923306c0acf5fa27574a15045cf6
edersonrincel <edersonrincel@gmail.com>
2025-08-09 21:57:11 -0300

    Teste novo estilo de mapas - map.js

--- js/modules/map.js
+++ js/modules/map.js
@@ -163,8 +163,16 @@ export function initializeMap() {
 
     map = L.map(mapContainer);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    /*L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);*/
+
+    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> — Map data © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: 'abcd',
+        minZoom: 1,
+        maxZoom: 16,
+        ext: 'jpg'
     }).addTo(map);
 
     markers = L.markerClusterGroup();
--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v1.54'; 
+    const CACHE_NAME = 'viagem-app-cache-v1.55'; 
     const URLS_TO_CACHE = [
         '/',
         '/index.html',

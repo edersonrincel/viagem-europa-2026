a8510df1e2f8c040e8058bf6ae509cff45fa6710
edersonrincel <edersonrincel@gmail.com>
2025-08-12 19:20:21 -0300

    Ajuste ícone dos cards dia-a-dia

--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v2.0'; 
+    const CACHE_NAME = 'viagem-app-cache-v2.01'; 
     const URLS_TO_CACHE = [
         '/',
         '/index.html',

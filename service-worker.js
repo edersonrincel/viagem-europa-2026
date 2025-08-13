af97ecc1ac0f7fb0727a7f9bf0fdecbb8bc129ca
edersonrincel <edersonrincel@gmail.com>
2025-08-13 20:23:35 -0300

    Teste faixa podcast

--- service-worker.js
+++ service-worker.js
@@ -41,7 +41,7 @@ try {
     });
 
     // --- Lógica de Caching e Ciclo de Vida ---
-    const CACHE_NAME = 'viagem-app-cache-v2.21'; 
+    const CACHE_NAME = 'viagem-app-cache-v2.22'; 
     const URLS_TO_CACHE = [
         '/',
         '/index.html',
